64a848bc4214fa8ec0db55ed5714e720ad63d1e4
PresidentComanch <ap.dissembler@gmail.com>
2021-12-19 14:59:40 +0200

    make refactoring

--- src/components/PokemonCardList/PokemonCardList.tsx
+++ src/components/PokemonCardList/PokemonCardList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Container, Grid, Pagination, Stack } from '@mui/material'
 import { useAppSelector } from '../../app/hooks'
